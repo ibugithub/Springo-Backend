f0efbb5d74a2f6552a123ee7990b49ac35c918fe
Md Ibrahim Hossain <outsideworkibrahim@gmail.com>
2024-04-15 04:42:33 +0600

    Add author in Story model

--- story/models.py
+++ story/models.py
@@ -1,10 +1,12 @@
 from django.db import models
 
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
+User = get_user_model()
 
 class Story(models.Model):
     name = models.CharField(max_length=100)
     story = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     def __str__(self):
         return self.name
